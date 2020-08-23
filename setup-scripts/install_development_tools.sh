27a7e912388d35a99fe036e655d568822ae114fe
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2020-08-24 00:41:53 +0200

    Updates

--- setup-scripts/install_development_tools.sh
+++ setup-scripts/install_development_tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Works only with ArchLinux
-sudo pacman -S git intellij-idea-community-edition pycharm-community-edition bless code geany
+sudo pacman -S intellij-idea-community-edition pycharm-community-edition bless code geany
